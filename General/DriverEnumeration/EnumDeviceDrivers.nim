d038c07443704b552b58f436032573b5439ab647
dk0m <hookirp@gmail.com>
2024-07-06 10:20:11 -0700

    MemoryScan & DriverEnum

---
+++ General/DriverEnumeration/EnumDeviceDrivers.nim
@@ -0,0 +1,47 @@
+import winim
+
+proc toStringA(chars: openArray[CHAR]): string =
+    for c in chars:
+        if c == '\0':
+            break
+        result.add((c))
+
+
+proc GetDriversBases*(): seq[LPVOID] =
+    var driverBases: array[1024, LPVOID]
+    var neededSize: DWORD
+
+    EnumDeviceDrivers(&driverBases[0], cast[DWORD](sizeof(driverBases)), &neededSize)
+    var amountOfDrvs = int(neededSize / sizeof(LPVOID))
+
+    return driverBases[0 .. amountOfDrvs - 1]
+
+
+proc GetDriverName*(driverBase: LPVOID): string =
+    var drvName: array[MAX_PATH, CHAR]
+    GetDeviceDriverBaseNameA(driverBase, &drvName[0], cast[DWORD](sizeof(drvName)))
+    return drvName.toStringA()
+
+proc GetDriverFileName*(driverBase: LPVOID): string =
+    var drvFileName: array[MAX_PATH, CHAR]
+    GetDeviceDriverFileNameA(driverBase, &drvFileName[0], cast[DWORD](sizeof(drvFileName)))
+    return drvFileName.toStringA()
+
+# Example Of Usage #
+
+#[
+
+var drvs = GetDriversBases()
+
+for drvBase in drvs:
+    var drvFileName = GetDriverFileName(drvBase)
+    var drvBaseName = GetDriverName(drvBase)
+    var drvBaseRepr = drvBase.repr()
+
+    echo "Driver Base: ", drvBaseRepr
+    echo "Driver Name: ", drvBaseName
+    echo "Driver File Path: ", drvFileName
+
+    echo "---------------------"
+
+]#
---
+++ General/MemoryScanning/MemScan.nim
@@ -0,0 +1,77 @@
+import winim, ptr_math
+import ../ProcessEnumeration/CreateToolHelp32
+
+proc GetProcessMemoryRegions*(hProcess: HANDLE): seq[MEMORY_BASIC_INFORMATION] =
+    var memRegions: seq[MEMORY_BASIC_INFORMATION]
+    var mbi: MEMORY_BASIC_INFORMATION
+    var baseAddr: LPVOID
+    
+    while VirtualQueryEx(hProcess, baseAddr, &mbi, cast[SIZE_T](sizeof(mbi))) != 0:
+        if (mbi.State and MEM_COMMIT) == MEM_COMMIT:
+            memRegions.add(mbi)
+        baseAddr = cast[LPVOID](cast[DWORD_PTR](mbi.BaseAddress) + mbi.RegionSize)
+
+    return memRegions
+
+
+proc ScanFor*[T](hProcess: HANDLE, memRegions: seq[MEMORY_BASIC_INFORMATION], targetValue: T): seq[LPVOID] =
+    for memRegion in memRegions:
+        var regionBase = memRegion.BaseAddress
+        var regionSize = memRegion.RegionSize
+
+        var regionBuffer = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, regionSize)
+        ReadProcessMemory(hProcess, regionBase, regionBuffer, regionSize, NULL)
+
+        for offset in countup(0, regionSize - sizeof(T), sizeof(T)):
+            var readValue = cast[ptr T](regionBuffer + offset)[]
+            if readValue == targetValue:
+                result.add(cast[LPVOID](regionBase + offset))
+            
+        HeapFree(GetProcessHeap(), 0, regionBuffer)
+
+
+proc ScanString*(hProcess: HANDLE, memRegions: seq[MEMORY_BASIC_INFORMATION], targetString: LPCSTR): seq[LPVOID] =
+    var stringLen = lstrlenA(targetString)
+
+    for memRegion in memRegions:
+        var regionBase = memRegion.BaseAddress
+        var regionSize = memRegion.RegionSize
+
+        var regionBuffer = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, regionSize)
+        ReadProcessMemory(hProcess, regionBase, regionBuffer, regionSize, NULL)
+
+        for offset in countup(0, regionSize - stringLen, stringLen):
+            var readValue = cast[LPCSTR](regionBuffer + offset)
+            if lstrcmpA(readValue, targetString) == 0:
+                result.add(cast[LPVOID](regionBase + offset))
+            
+        HeapFree(GetProcessHeap(), 0, regionBuffer)
+
+var notepadId = GetProcessIdFromName("notepad.exe")
+var hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, notepadId)
+
+
+
+# Value Scan #
+
+
+#[
+
+var memRegions = GetProcessMemoryRegions(hProcess)
+var addresses = ScanFor[int](hProcess, memRegions, 4)
+
+for addr in addresses:
+    echo "Found Value 4 At Address: ", addr.repr()
+]#
+
+
+# String Scan #
+
+#[
+
+var memRegions = GetProcessMemoryRegions(hProcess)
+var addresses = ScanString(hProcess, memRegions, "VirtualAlloc")
+for addr in addresses:
+    echo "Found String 'VirtualAlloc' At Address: ", addr.repr()
+]#
+
