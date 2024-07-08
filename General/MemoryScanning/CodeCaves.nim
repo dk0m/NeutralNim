f0bc3f414cd5f39458544d6049f7457e65c2b49a
dk0m <hookirp@gmail.com>
2024-07-08 13:40:33 -0700

    Modularize Code Caves

--- General/MemoryScanning/CodeCaves.nim
+++ General/MemoryScanning/CodeCaves.nim
@@ -1,29 +1,33 @@
 import winim, ../PeParsing/Parser
 
-var peNtdll = parsePe("C:\\Windows\\System32\\ntdll.dll")
-var minSize = 50
+type
+    Cave* = object
+     Section*: string
+     RawAddress*: LPVOID
+     VirtualAddress*: LPVOID
 
-for section in peNtdll.Sections:
-    var sectionName = toStringFromByteArray(section.Name)
-    var ptrToData = section.PointerToRawData
-    
-    var sectionDataBase = cast[DWORD_PTR](peNtdll.ImageBase + ptrToData)
-    var sectionDataSize = section.SizeOfRawData
-    var freeBytes = 0
-    for i in 0..sectionDataSize:
-        var readByte = cast[PBYTE](sectionDataBase + cast[DWORD](i))[]
-        if readByte == 0x0:
-            freeBytes += 1
-        else:
-            if freeBytes > minSize:
-                var caveStartAddress = cast[LPVOID](ptrToData + i - freeBytes - 1)
-                var caveVa = cast[LPVOID](peNtdll.Headers.OptHeader.ImageBase + ptrToData + i - freeBytes - 1)
-                echo "======== Found Cave ========"
-                echo "Section: " & sectionName
-                echo "Cave Size: " & $freeBytes
-                echo "Cave Raw Address: " & repr(caveStartAddress)
-                echo "Cave Virtual Address: " & repr(caveVa)
-                echo "=========================\n\n"
+proc findCaves*(peNtdll: PeFile, minSize: int): seq[Cave] =
+    for section in peNtdll.Sections:
+        var sectionName = toStringFromByteArray(section.Name)
+        var ptrToData = section.PointerToRawData
+        
+        var sectionDataBase = cast[DWORD_PTR](peNtdll.ImageBase + ptrToData)
+        var sectionDataSize = section.SizeOfRawData
+        var freeBytes = 0
+        for i in 0..sectionDataSize:
+            var readByte = cast[PBYTE](sectionDataBase + cast[DWORD](i))[]
+            if readByte == 0x0:
+                freeBytes += 1
+            else:
+                if freeBytes > minSize:
+                    var caveStartAddress = cast[LPVOID](ptrToData + i - freeBytes)
+                    var caveVa = cast[LPVOID](peNtdll.Headers.OptHeader.ImageBase + ptrToData + i - freeBytes)
+                    result.add(
+                        Cave(
+                            Section: sectionName,
+                            RawAddress: caveStartAddress,
+                            VirtualAddress: caveVa
+                        )
+                    )
 
-            freeBytes = 0   
-    
+                freeBytes = 0   
